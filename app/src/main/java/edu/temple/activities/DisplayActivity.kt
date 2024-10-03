5e89bad4da8493a2d328255f63e226d5241153d9
Ian Thorpe <tup56195@temple.edu>
2024-10-03 15:10:21 -0400

    Implemented result function of TextSizeActivity

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -1,9 +1,11 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 
 class DisplayActivity : AppCompatActivity() {
 
@@ -14,12 +16,22 @@ class DisplayActivity : AppCompatActivity() {
     private lateinit var lyricsDisplayTextView: TextView
     private lateinit var textSizeSelectorButton: Button
 
+    val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        if(it.resultCode == RESULT_OK){
+            it.data?.apply {
+                lyricsDisplayTextView.textSize = getFloatExtra(RESULT_KEY, 22f)
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display)
 
         lyricsDisplayTextView = findViewById(R.id.lyricsDisplayTextView)
         textSizeSelectorButton = findViewById(R.id.textSizeSelectorButton)
-
+        textSizeSelectorButton.setOnClickListener{
+            launcher.launch(Intent(this@DisplayActivity, TextSizeActivity::class.java))
+        }
     }
 }
